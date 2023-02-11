4334c07968d30bf75e6d6e017121c0ab3727a606
marshmallowlands <2145136703@qq.com>
2023-02-11 10:44:07 +0800

    测试版 1.0

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     var mapcode = document.querySelector('#inputMapCode');
     var jump = document.querySelector('#jump-to-rockstar')
-    function jump(onClick) {
+    jump.addEventListener('click', function () {
         document.location.href = 'https://socialclub.rockstargames.com/job/gtav' + mapcode.value;
-    }
+    })
 }
