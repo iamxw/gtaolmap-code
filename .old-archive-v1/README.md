ab738b1df6d134be45c688da10931f3820a6b6c3
marshmallowlands <2145136703@qq.com>
2023-03-14 09:13:47 +0800

    文件整理

--- .old-archive-v1/README.md
+++
@@ -1,2 +0,0 @@
-# gta5map-code
-GTA5 地图代码快速补全
--- .old-archive-v1/js/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    var mapcode = document.querySelector('#inputMapCode');
-    var jump = document.querySelector('#jump-to-rockstar')
-    jump.addEventListener('click', function () {
-        document.location.href = 'https://socialclub.rockstargames.com/job/gtav' + mapcode.value;
-    })
-}
