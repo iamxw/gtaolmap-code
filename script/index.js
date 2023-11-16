33ec0fae9b9c917feebd40a83e96720ebff7c0f1
marshmallowlands <2145136703@qq.com>
2023-11-17 01:14:43 +0800

    小修小改

--- script/index.js
+++ script/index.js
@@ -1,12 +1,15 @@
 window.onload = function () {
     let mapCode = document.querySelector('#code');
     let jump = document.querySelector('#submit-to-rockstar-gta-v-job');
+    let jump_Tips = document.querySelector('#tips');
+    jump_Tips.style.color = '#49d590';
     // 判断字符是否是空的
     jump.addEventListener('click', function () {
         if (mapCode.value === '') {
             jump.style.backgroundColor = '#dc4846';
             jump.style.color = '#fff';
-            jump.textContent = '空文本框';
+            jump_Tips.style.color = '#DC4846FF';
+            jump_Tips.textContent = 'The text box is empty, please check and try again';
             mapCode.style.borderColor = '#dc4846';
         } else {
             window.open('https://socialclub.rockstargames.com/job/gtav/' + mapCode.value);
@@ -14,8 +17,10 @@ window.onload = function () {
     })
     mapCode.addEventListener('click', function () {
         mapCode.style.borderColor = '#FFFFFF7F';
-        jump.textContent = '搜索差事';
+        jump.textContent = '搜索此差事';
         jump.style.color = '#000';
         jump.style.backgroundColor = '#fff';
+        jump_Tips.style.color = '#49d590';
+        jump_Tips.textContent = 'Enter the code in the input box, then click "Search for this job" button.';
     })
 }
